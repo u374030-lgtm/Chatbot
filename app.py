93288cb6b956d30cd78f083d3c25ecf4ac84446f
u374030-lgtm <u374030@gmail.com>
2025-09-19 20:58:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import os
+from flask import Flask, render_template, request
+from faq_bot import faq_bot
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    answer = ""
+    if request.method == "POST":
+        user_input = request.form["question"]
+        answer = faq_bot(user_input)
+    return render_template("index.html", answer=answer)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
